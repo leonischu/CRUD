467f4987455a6ab15c96d3679e4230b1b36b7c9f
leonischu <nischal.201423@ncit.edu.np>
2025-12-10 16:27:05 +0545

    Collections

---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+/*
+class Program
+{
+    public static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        //list.Add(1);
+        list.Add("Hello");
+        //list.Add(true);
+        foreach (string o in list)
+            Console.WriteLine(o);
+
+        List<string> fruits = new List<string>();
+        fruits.Add("Apple");
+        fruits.Add("Mango");
+        fruits.Add("Orange");
+
+        list.AddRange(fruits);
+
+        list.Insert(0, "Banana");
+        list[0] = "tomato";
+        list.RemoveAt(1);
+        foreach (string o in list)
+        {
+            Console.WriteLine(o);
+        }
+
+        //Console.WriteLine(list.Capacity);
+        //Console.WriteLine(list.Count);
+    }
+
+}
+*/
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+
+        dict.Add(1, "Hello");
+        dict.Add(10, "Orange");
+        dict.Add(15, "Mango");
+
+        dict.Remove(1);
+
+        if (dict.ContainsKey(10))
+            Console.WriteLine("sucess");
+        else
+            Console.WriteLine("Failure");
+            foreach (var o in dict)
+                Console.WriteLine(o);
+        
+
+    }
+
+}
