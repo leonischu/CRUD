3996b4878ccfd2380bfe1de269a3f84012d180ad
leonischu <nischal.201423@ncit.edu.np>
2025-12-19 16:41:00 +0545

    Role Privilage

---
+++ CollegeApp/CollegeApp/Data/Role.cs
@@ -0,0 +1,28 @@
+﻿namespace CollegeApp.Data
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+
+        public string Password { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public int UserType { get; set; }
+
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+        public virtual ICollection<RolePrivileges> { get; set; }
+
+
+
+    }
+}
---
+++ CollegeApp/CollegeApp/Data/RolePrivilege.cs
@@ -0,0 +1,25 @@
+﻿namespace CollegeApp.Data
+{
+    public class RolePrivilege
+    {
+        public int Id { get; set; }
+        public string RolePrivilegeName { get; set; }
+
+
+        public string Password { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public int UserType { get; set; }
+
+
+        public bool IsActive { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime ModifiedDate { get; set; }
+
+    }
+}
